f6416ef5bb662b2c2a8b8b963c2c2112eea36edc
yunyechan <dpcks9893@naver.com>
2025-03-28 09:16:24 +0900

    [Feat] 시큐리티 인가 설정

---
+++ src/main/java/com/example/securitypractice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.securitypractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+    httpSecurity.authorizeHttpRequests(auth ->
+        auth.requestMatchers("/login", "/join").permitAll()
+            .requestMatchers("/").permitAll()
+            .requestMatchers("/my/**").hasAnyRole("USER", "ADMIN")
+            .requestMatchers("/admin").hasRole("ADMIN")
+            .anyRequest().authenticated()
+    );
+
+    return httpSecurity.build();
+  }
+
+}
