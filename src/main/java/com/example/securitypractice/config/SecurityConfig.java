5af3e9313238b29fa6eb1e5aca0cd898a5c1f4cf
yunyechan <dpcks9893@naver.com>
2025-03-28 09:16:37 +0900

    [Feat] 시큐리티 로그인 설정

--- src/main/java/com/example/securitypractice/config/SecurityConfig.java
+++ src/main/java/com/example/securitypractice/config/SecurityConfig.java
@@ -21,6 +21,13 @@ public class SecurityConfig {
             .anyRequest().authenticated()
     );
 
+    httpSecurity.formLogin(auth -> auth.loginPage("/login") // 권한 없는 페이지 접속 시 로그인 페이지로 이동
+        .loginProcessingUrl("/loginProc") // 로그인 처리 진행
+        .permitAll()
+    );
+
+    httpSecurity.csrf(auth -> auth.disable()); // 로그인 시 토큰을 같이 보내줘야하지만, 추후 구현하므로 임시 장애 처리
+
     return httpSecurity.build();
   }
 
